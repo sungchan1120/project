6938797941e1dcbad749e43e76631b141d43f30e
sungchan1120 <yuyiyo1000@gmail.com>
2023-07-18 18:11:05 +0900

    http

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class Loading extends StatefulWidget {
   @override
@@ -11,21 +12,35 @@ class _LoadingState extends State<Loading> {
   void initState() {
     super.initState();
     getLocation();
+    fetchData();
   }
 
   void getLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();
-    Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch (e) {
+      print('인터넷 연결 문제');
+    }
+  }
+
+  void fetchData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+    }
+    print(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
+        child: ElevatedButton(
           onPressed: () {
             getLocation();
           },
