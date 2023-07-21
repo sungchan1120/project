c2494717bf629a3d1215dcc1d7ff692ca5161fe5
sungchan1120 <yuyiyo1000@gmail.com>
2023-07-21 22:06:48 +0900

    light intensity drizzle

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -32,8 +33,9 @@ class _LoadingState extends State<Loading> {
         'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
     if (response.statusCode == 200) {
       String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      print(myJson);
     }
-    print(response.body);
   }
 
   @override
