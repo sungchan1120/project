81eb67803ce694b029191d2eec0b6b21329307af
sungchan1120 <yuyiyo1000@gmail.com>
2023-08-08 22:45:32 +0900

    4강 25분 svg 추가 내용

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:intl/intl.dart';
 
 class WeatherScreen extends StatefulWidget {
   WeatherScreen({this.parseWeatherData});
@@ -11,6 +14,7 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   late String cityName;
   late int temp;
+  var date = DateTime.now();
   @override
   void initState() {
     super.initState();
@@ -26,10 +30,30 @@ class _WeatherScreenState extends State<WeatherScreen> {
     print(cityName);
   }
 
+  String getSystemTime() {
+    var now = DateTime.now();
+    return DateFormat("h:m a").format(now);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text(''),
+        // title: Text(''),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.near_me),
+          onPressed: () {},
+          iconSize: 30,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.location_searching),
+            onPressed: () {},
+            iconSize: 30,
+          )
+        ],
       ),
       body: Container(
         child: Stack(
@@ -37,6 +61,92 @@ class _WeatherScreenState extends State<WeatherScreen> {
             Image.asset(
               'assets/images/background.jpg',
               fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 150,
+                              ),
+                              Text(
+                                'Seoul',
+                                style: GoogleFonts.lato(
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              Row(
+                                children: [
+                                  TimerBuilder.periodic(
+                                    (Duration(minutes: 1)),
+                                    builder: (context) {
+                                      print('${getSystemTime}');
+                                      return Text(
+                                        '${getSystemTime()}',
+                                        style: GoogleFonts.lato(
+                                            fontSize: 16, color: Colors.white),
+                                      );
+                                    },
+                                  ),
+                                  Text(
+                                    DateFormat(' - EEEE').format(date),
+                                    style: GoogleFonts.lato(
+                                        fontSize: 16, color: Colors.white),
+                                  ),
+                                  Text(
+                                    DateFormat('d MMM, yyy').format(date),
+                                    style: GoogleFonts.lato(
+                                        fontSize: 16, color: Colors.white),
+                                  )
+                                ],
+                              )
+                            ]),
+                        Column(
+                          children: [
+                            Text(
+                              '18\u2103',
+                              style: GoogleFonts.lato(
+                                  fontSize: 85,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            Row(
+                              children: [
+                                SvgPicture.asset(
+                                    'assets/images/Cloud-Lightning.svg')
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Divider(
+                        height: 15,
+                        thickness: 2,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [Text('미세먼지')],
+                  )
+                ],
+              ),
             )
           ],
         ),
