89360c9c642df51906172275a4c5f9c5f81e53e2
sungchan1120 <yuyiyo1000@gmail.com>
2023-08-23 22:37:39 +0900

    업그레이드 전

---
+++ lib/model/model.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Model {
+  Widget getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return SvgPicture.asset(
+        'assets/images/colud_lightning.svg',
+      );
+    } else if (condition < 600) {
+      return SvgPicture.asset(
+        'assets/images/colud_snow_alt.svg',
+      );
+    } else if (condition == 800) {
+      return SvgPicture.asset(
+        'assets/images/sun.svg',
+      );
+    } else if (condition <= 804) {
+      return SvgPicture.asset(
+        'assets/images/colud_sun.svg',
+      );
+    }
+    return Text("Weather icon not available");
+  }
+}
